b9b3fbe49d1eb76a06eace2174f7d10530323fa8
ex_likx5 <ex_likx5@partner.midea.com>
2021-11-19 16:26:25 +0800

    歌单详情

--- src/views/page/playlist/detail.vue
+++ src/views/page/playlist/detail.vue
@@ -52,6 +52,37 @@
       </div>
     </div>
   </div>
+  <div class="main">
+    <van-skeleton title
+                  :row="20"
+                  :loading="songLoading">
+      <van-list v-model:loading="loading"
+                :finished="finished"
+                @load="onload">
+
+        <div class="song-content">
+          <ul class="song-list">
+            <li class="song-item"
+                v-for="(item, i) in songList"
+                :key="item.id">
+              <b class="song-index">{{Number(i) + 1}}</b>
+              <div class="song-item-right">
+                <h4 class="song-name">
+                  <span> {{item.name}} </span>
+                  <span v-if="item.alia[0]">({{item.alia[0]}})</span>
+                </h4>
+                <p class="singer">
+                  <span class="sq"
+                        v-if="[1,2].includes(item.no) ">SQ</span>
+                  <span>{{item.ar[0].name}} - {{item.al.name}}</span>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </van-list>
+    </van-skeleton>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, watch, computed } from "vue";
@@ -62,6 +93,7 @@ import {
   Skeleton as VanSkeleton,
   NoticeBar as VanNoticeBar,
   Icon as VanIcon,
+  List as VanList,
 } from "vant";
 import {
   useRoute,
@@ -71,36 +103,70 @@ import {
 } from "vue-router";
 const route = useRoute();
 const router = useRouter();
-console.log("route", route);
 interface playHeader {
   coverImgUrl: string;
   name: string;
   description: string;
   commentCount: string;
-  playCount: number,
-  shareCount: string,
+  playCount: number;
+  shareCount: string;
   creator: {
     backgroundUrl: string;
     nickname: string;
   };
 }
-let playListHeader = ref<playHeader>({
+let defHead = {
   coverImgUrl: "",
   name: "",
   description: "",
   playCount: 0,
-  shareCount: '',
-  commentCount: '',
+  shareCount: "",
+  commentCount: "",
   creator: {
     backgroundUrl: "",
     nickname: "",
   },
-});
+};
+let playListHeader = ref<playHeader>(defHead);
 let oldParams: any = {
   id: "",
 };
 let headLoading = ref(true);
+let songLoading = ref(false);
+interface songList {
+  [item: string]: {
+    id: string;
+    copyright: number;
+    cp: number;
+    fee: number;
+    no: number;
+    al: {
+      id: string;
+      name: string;
+    };
+    alia: {
+      [item: string]: {};
+    };
+    ar: {
+      [item: string]: {
+        name: string;
+      };
+    };
+    name: string;
+  };
+}
+let songList = ref<[] | songList>([]);
 
+const loading = ref(false);
+const finished = ref(false);
+const onload = () => {
+  console.log(page.value)
+  // const { songs } = await getSong(songId);
+  // songList.value = songs;
+  loading.value = true
+};
+let page = ref({})
+// 初始化歌单详情
 const _initDetailData = async (id: string) => {
   const detailData: any = await axios.get({
     url: "/playlist/detail",
@@ -109,8 +175,15 @@ const _initDetailData = async (id: string) => {
     },
   });
   const { playlist } = detailData;
-  console.log("playlist", playlist);
-  const { coverImgUrl, name, creator, description, playCount ,commentCount, shareCount} = playlist;
+  const {
+    coverImgUrl,
+    name,
+    creator,
+    description,
+    playCount,
+    commentCount,
+    shareCount,
+  } = playlist;
   playListHeader.value = {
     coverImgUrl,
     commentCount,
@@ -118,31 +191,91 @@ const _initDetailData = async (id: string) => {
     creator,
     description,
     shareCount,
-    playCount
+    playCount,
   };
   headLoading.value = false;
+  return playlist;
 };
 
-const initData = (id: string) => {
-  _initDetailData(id);
+// 根据ids 去获取对应的歌
+const getSong = async (ids: string) => {
+  if (ids.length < 1) {
+    return;
+  }
+  const detailData: any = await axios.get({
+    url: "/song/detail",
+    data: {
+      ids,
+    },
+  });
+  songLoading.value = false;
+  return detailData;
 };
 
+// 处理trackIds数组 转成一个字符串
+const handlerids = (ids: any) => {
+  let songId: any = [];
+  ids.forEach((item: any) => {
+    songId.push(item.id);
+  });
+  return songId.join(",");
+};
+
+// 按着20个整理分组
+const handlerPage = (list:any, num:number) => {
+  let i = 0
+  let _key = 0
+  let _arr:any =  []
+  let _obj:any = {}
+
+  list.forEach((item:any) => {
+    i++ 
+    _arr.push(item)
+    if (i === num) {
+      i = 0
+      _obj[_key] = _arr
+      _key++
+      _arr = []
+    }
+  })
+  _obj[_key] = _arr
+  return _obj
+}
+const initData = async (id: string) => {
+  // 获取歌单详情
+  const playlist = await _initDetailData(id);
+  // 把trackIds数组的id 整成字符串 xxx,xxx
+  
+  const songId = handlerids(playlist.trackIds);
+  page = handlerPage(playlist.trackIds, 20)
+  // 获取歌列表
+  // const { songs } = await getSong(songId);
+  // songList.value = songs;
+};
+
+// 监听入参有没有变化
 watch(
   () => route.params,
   (val, oval) => {
     if (route.path.indexOf("/playlist/detail/") < 0) {
       return;
     }
+
+    // 判断是不是重复进来的歌单 如果是重复的 就不去获取接口 直接显示
     if (oldParams?.id === val?.id) {
       headLoading.value = false;
+      songLoading.value = false;
       return;
     }
+    headLoading.value = true;
+    songLoading.value = true;
+    playListHeader.value = defHead;
+    songList.value = [];
     initData(val.id as string);
   }
 );
 onBeforeRouteLeave((to, from) => {
   oldParams = from.params;
-  headLoading.value = true;
 });
 
 initData(route.params.id as string);
@@ -250,6 +383,53 @@ const onClickLeft = () => {
       color: var(--whiteColor);
       line-height: 24px;
       padding-left: 5px;
+    }
+  }
+}
+.main::v-deep {
+  .song-item {
+    display: flex;
+    padding-bottom: 20px;
+    &:first-child {
+      margin-top: 20px;
+    }
+    &:active {
+      background: #f7f7f7;
+    }
+    .song-index {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 45px;
+      color: #b4b4b4;
+      font-size: 18px;
+      font-weight: normal;
+    }
+    .song-item-right {
+      flex: 1;
+    }
+    .song-name {
+      margin: 10px 0;
+      margin-bottom: 5px;
+      font-size: 16px;
+    }
+    .singer {
+      margin: 0;
+      color: #878787;
+      display: flex;
+      .sq {
+        color: #f07473;
+        font-size: 12px;
+        height: 13px;
+        border: 1px solid #f39897;
+        background: #fff2f2;
+        border-radius: 3px;
+        padding: 0 2px;
+        transform: scale(0.8);
+        display: block;
+        margin-top: 3px;
+        margin-right: 3px;
+      }
     }
   }
 }
