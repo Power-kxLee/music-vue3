87a13a5f84bbc2ea2e64fa1875b5b8a91ef0cab1
ex_likx5 <ex_likx5@partner.midea.com>
2021-12-15 16:05:27 +0800

    我的页面开发

--- src/views/page/home/me.vue
+++ src/views/page/home/me.vue
@@ -7,7 +7,7 @@
           <van-image width="80"
                      height="80"
                      round
-                     :src="userData.profile.backgroundUrl" />
+                     :src="userImg" />
         </div>
         <div class="user-detail">
           <h3 class="user-name">
@@ -37,7 +37,6 @@
         <div class="boxdiv">
           <header>音乐品味</header>
           <van-cell is-link>
-            <!-- 使用 title 插槽来自定义标题 -->
             <template #title>
               <div class="boxdivlist">
                 <div class="icon">
@@ -52,7 +51,6 @@
             <template #right-icon></template>
           </van-cell>
           <van-cell is-link>
-            <!-- 使用 title 插槽来自定义标题 -->
             <template #title>
               <div class="boxdivlist">
                 <div class="icon">
@@ -60,12 +58,34 @@
                 </div>
                 <div class="content">
                   <h5>我喜欢的音乐</h5>
-                  <section>累计听歌{{userData.likeNum}}首</section>
+                  <section>{{userData.likeNum}}首</section>
                 </div>
               </div>
             </template>
             <template #right-icon></template>
           </van-cell>
+        </div>
+        <div class="boxdiv">
+          <header>收藏的歌单</header>
+          <van-cell is-link
+                    v-for="item in playListData"
+                    :key="item.id">
+            <template #title>
+              <div class="boxdivlist">
+                <div class="icon">
+                  <van-image width="60"
+                             height="60"
+                             :src="item.coverImgUrl" />
+                </div>
+                <div class="content">
+                  <h5>{{item.name}}</h5>
+                  <section>{{item.trackCount}}首, by {{item.creator.nickname}}, 播放{{handlePlayCount(item.playCount)}}</section>
+                </div>
+              </div>
+            </template>
+            <template #right-icon></template>
+          </van-cell>
+
         </div>
       </template>
     </main>
@@ -81,7 +101,8 @@ import placesMap from "@/data/placesMap";
 const store = useStore();
 const userData: any = ref({});
 const _placesMap: any = placesMap;
-
+const userImg = ref("");
+const playListData: any = ref({});
 const isLogin = () => {
   const _len = Object.keys(store.state.loginStatus).length;
   if (_len > 0 || localStorage.getItem("loginStatus")) {
@@ -89,19 +110,20 @@ const isLogin = () => {
       _len > 0
         ? store.state.loginStatus
         : JSON.parse(localStorage.getItem("loginStatus") || "");
-    return val.data
+    return val.data;
   }
-  return {}
-}
+  return {};
+};
+let loginData = isLogin(); // 获取登录后的内容
 const init = () => {
   // 获取用户的详情内容
-  initDetail()
-  initLive()
+  initDetail();
+  // 获取喜欢的音乐数量
+  initLive();
+  playList();
 };
 
 const initDetail = async () => {
-   // 获取登录后的内容
-  let loginData = isLogin()
   // 获取用户详情
   const detailData = await axios.get({
     url: "/user/detail",
@@ -121,19 +143,52 @@ const initDetail = async () => {
   const year: any = new Date(loginData.profile.birthday)
     .getFullYear()
     .toString();
-    // 比如是1991年 那么就返回 91年
+  // 比如是1991年 那么就返回 91年
   userData.value.birthday = year.substring(year.length - 2, year.length);
-}
+  userImg.value = userData.value.profile.backgroundUrl;
+};
 const initLive = async () => {
-  let loginData = isLogin()
-  const {ids}:any = await axios.get({
-    url: '/likelist',
-    data:{
-      uid:loginData.profile.userId
+  const { ids }: any = await axios.get({
+    url: "/likelist",
+    data: {
+      uid: loginData.profile.userId,
+    },
+  });
+  userData.value.likeNum = ids.length;
+  console.log(userData.value);
+};
+// 保留多少位小数, 不会四舍五入
+// 如果是整数会直接返回
+const toFixed = (val: number, count: number) => {
+  const [l, r]: any = val.toString().split(".");
+  const nr = r ? (r.length > count ? r.substr(0, count) : r) : "";
+  return l + "." + nr;
+};
+const handlePlayCount = (playCount: number) => {
+  const _playCount: string = playCount.toString();
+  let newPlayCount: any =
+    _playCount.length >= 9
+      ? toFixed(Number(_playCount) / 100000000, 1) + "亿"
+      : toFixed(Number(_playCount) / 10000, 1) + "万";
+  return newPlayCount;
+};
+const playList = async () => {
+  const { playlist }: any = await axios.get({
+    url: "/user/playlist",
+    data: {
+      uid: loginData.profile.userId,
+    },
+  });
+  let defaultNum = 2; // 默认值显示头2个
+  const newPlayList = playlist.filter((item: any) => {
+    if (item.subscribed && defaultNum) {
+      defaultNum--;
+      return item;
     }
-  })
-  userData.value.likeNum = ids.length
-}
+  });
+  playListData.value = newPlayList;
+};
+
 init();
 </script>
 <style lang="scss" scoped>
@@ -206,9 +261,14 @@ init();
         display: flex;
         flex-direction: column;
         justify-content: center;
+        flex: 1;
         h5 {
           font-size: 14px;
           font-weight: normal;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          width: calc(100vw - 120px);
         }
         section {
           color: rgb(133, 133, 133);
@@ -222,6 +282,7 @@ init();
     .user-detail {
       margin-left: 20px;
       display: flex;
+
       flex-direction: column;
       justify-content: center;
       .user-name {
